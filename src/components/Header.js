7d4da171b3e24e2fe174b83f97ac5a1b8683a782
camilla-bachna <camilla.bachna@gmail.com>
2021-02-18 17:41:32 +0100

    create new branch and import header stylesheet

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,11 @@
-import "../stylesheets/App.scss";
-import headerLogo from "../images/logo-tree2GIF.gif";
+import '../stylesheets/layout/_header.scss';
+import headerLogo from '../images/logo-tree2GIF.gif';
 
 function Header() {
   return (
-       <header className="header central-column">
-            <img
-            className="header__logo"
-            src={headerLogo}
-            alt="logo awesome cards"
-            />
-      </header>
-
+    <header className="header central-column">
+      <img className="header__logo" src={headerLogo} alt="logo awesome cards" />
+    </header>
   );
 }
 export default Header;
