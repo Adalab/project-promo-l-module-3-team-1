e256a661f35aac95cb25c71167a72b9e2523013f
Anadbh <anafruiz20@gmail.com>
2021-02-18 21:07:10 +0100

    Component Reset Card

---
+++ src/components/Card/ResetButton.js
@@ -0,0 +1,13 @@
+import "../stylesheets/App.scss";
+
+function ResetButton() {
+  return (
+    <div className="photo-container">
+      <div className="photo-button ">
+        <button className="photo-button__link js-reset">Reset</button>
+        <i className="fa fa-trash" aria-hidden="true"></i>
+      </div>
+    </div>
+  );
+}
+export default ResetButton;
