28b97b4c914d8ed8b0186cc5ed2b1669e837009c
Anadbh <anafruiz20@gmail.com>
2021-02-19 14:33:57 +0100

    Merge branch form and footer in dev

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,53 +1,52 @@
 import "../../stylesheets/App.scss";
-import ResetButton from './ResetButton';
+import ResetButton from "./ResetButton";
 
 function Card() {
   return (
     <section className="photo">
-      <ResetButton/>
+      <ResetButton />
 
-        <article className="photocard js-card palette-1">
-          <div className="container">
-            <div className="tittlescontainer">
-              <h2 className=" photocard__title js-preview-name js-card-name js-input-text">
-                Nombre apellido
-              </h2>
-              <p className=" photocard__title2 js-preview-position js-card-job js-input-text">
-                Front-end developer
-              </p>
-            </div>
+      <article className="photocard js-card palette-1">
+        <div className="container">
+          <div className="tittlescontainer">
+            <h2 className=" photocard__title js-preview-name js-card-name js-input-text">
+              Nombre apellido
+            </h2>
+            <p className=" photocard__title2 js-preview-position js-card-job js-input-text">
+              Front-end developer
+            </p>
           </div>
-          <div className="photocard__img js__profile-image"></div>
-          <ul className="photocard__list">
-            <li className="photocard__list--item">
-              <a
-                href=""
-                className=" photocard__list--link icon fa fa-mobile js-card-phone  js-input-text"
-              ></a>
-            </li>
+        </div>
+        <div className="photocard__img js__profile-image"></div>
+        <ul className="photocard__list">
+          <li className="photocard__list--item">
+            <a
+              href=""
+              className=" photocard__list--link icon fa fa-mobile js-card-phone  js-input-text"
+            ></a>
+          </li>
 
-            <li className="photocard__list--item">
-              <a
-                href=""
-                className=" photocard__list--link icon fa fa-envelope js-card-email  js-input-text"
-              ></a>
-            </li>
+          <li className="photocard__list--item">
+            <a
+              href=""
+              className=" photocard__list--link icon fa fa-envelope js-card-email  js-input-text"
+            ></a>
+          </li>
 
-            <li className="photocard__list--item">
-              <a
-                href=""
-                className=" photocard__list--link js-card-linkedin icon fa fa-linkedin  js-input-text"
-              ></a>
-            </li>
-            <li className="photocard__list--item">
-              <a
-                href=""
-                className=" photocard__list--link js-card-github icon fa fa-github-alt  js-input-text"
-              ></a>
-            </li>
-          </ul>
-        </article>
-      </div>
+          <li className="photocard__list--item">
+            <a
+              href=""
+              className=" photocard__list--link js-card-linkedin icon fa fa-linkedin  js-input-text"
+            ></a>
+          </li>
+          <li className="photocard__list--item">
+            <a
+              href=""
+              className=" photocard__list--link js-card-github icon fa fa-github-alt  js-input-text"
+            ></a>
+          </li>
+        </ul>
+      </article>
     </section>
   );
 }
