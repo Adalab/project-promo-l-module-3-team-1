7da35ff7f15ef639e35331284b06363ec0c3bce2
Sagramielgo <sagramielgo@gmail.com>
2021-02-26 20:43:11 +0100

    add ev.preventDefault in Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,11 +1,13 @@
-import Fill from "./Fill/Fill";
-import Share from "./Share/Share";
-import Design from "./Design/Design";
-import Collapsable from "./Collapsable";
+import Fill from './Fill/Fill';
+import Share from './Share/Share';
+import Design from './Design/Design';
 
 function Form(props) {
+  const handleForm = (ev) => {
+    ev.preventDefault();
+  };
   return (
-    <form className="main-cards__sections--form">
+    <form onSubmit={handleForm} className="main-cards__sections--form">
       <Design />
       <Fill name={props.name} job={props.job} handleInput={props.handleInput} />
       <Share />
